fbb920c469016b7ae8de4c710d90c13d4c1e61dd
AtikTF <atiktuquerres@gmail.com>
2025-11-29 20:05:12 -0500

    Feat: Implementar login y registro del estudiante

---
+++ src/auth/domain/models/Estudiante.ts
@@ -0,0 +1,14 @@
+export interface Estudiante {
+  id_estudiante: number
+  id_profesor?: number
+  codigo_unico: number
+  primernombre: string
+  segundo_nombre: string
+  primer_apellido: string
+  segundo_apellido: string
+  correo_electronico: string
+  contrasenia: string
+}
+
+export type EstudianteInput = Omit<Estudiante, 'id_estudiante'>
+export type EstudiantePublic = Omit<Estudiante, 'contrasenia'>
